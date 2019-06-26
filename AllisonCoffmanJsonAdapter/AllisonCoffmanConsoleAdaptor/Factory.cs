3e97256a5ec727380053eaa9f288752ad3538a85
mayankanshu <writetomayankp@gmail.com>
2019-06-26 14:51:26 +0530

    Create Factory.cs

---
+++ AllisonCoffmanJsonAdapter/AllisonCoffmanConsoleAdaptor/Factory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace AllisonCoffmanConsoleAdaptor
+{
+    class Factory
+    {
+        public DataSet GetRetailerName(int id)
+        {
+            string RetailerName = string.Empty;
+            try
+            {
+                string ConnectionString = Convert.ToString(ConfigurationManager.ConnectionStrings["DefaultConnection"]);
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                //   string query = "spGetRetailerThirdPartyIntegrationDetailsDummy";
+                string query = "spGetRetailerThirdPartyIntegrationDetails";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@thirdPartyIntegrationId", id);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
