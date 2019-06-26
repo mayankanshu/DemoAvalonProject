9cb38b30ad0f367961209a7a1a20e291182702e7
mayankanshu <writetomayankp@gmail.com>
2019-06-26 14:51:10 +0530

    Create ReponseDataToAvalonData.cs

---
+++ AllisonCoffmanJsonAdapter/AllisonCoffmanConsoleAdaptor/Model/ReponseDataToAvalonData.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllisonCoffmanConsoleAdaptor.Model
+{
+    public class ReponseDataToAvalonData
+    {
+        public string SR { get; set; }
+        public string PRODUCT_STYLE_ID { get; set; }
+        public string SKU_ID { get; set; }
+        public string PRODUCT_NAME { get; set; }
+        public string PRODUCT_TAG { get; set; }
+        // public string Product_item_type { get; set; }
+        public string IMAGE_NAME_1 { get; set; }
+        public string IMAGE_NAME_2 { get; set; }
+        public string IMAGE_NAME_3 { get; set; }
+        public string PRODUCT_VIDEO { get; set; }
+        public string WEB_SHORT_DESCRIPTION { get; set; }
+        public string WEB_DETAIL_DESCRIPTION { get; set; }
+        public string PRICE_DISPLAY { get; set; }
+        // public bool CallForPrice { get; set; }
+        public string IF_NO_THEN_MESSAGE { get; set; }
+        //public string CallForPriceMessage { get; set; }
+        public string COST_PRICE { get; set; }
+        public string SELL_PRICE { get; set; }
+        public string SALE_PRICE { get; set; }
+        public float SELL_PRICE_PERCENTAGE { get; set; }
+        public float SALE_PRICE_PERCENTAGE { get; set; }
+        public string JEWELRY_TYPE { get; set; }
+        public string PRICE { get; set; }
+        public string SALE_PRICE_START_DATE { get; set; }
+        public string SALE_PRICE_END_DATE { get; set; }
+
+
+        //******************Jewelery Specification****************************//
+        public string DELIVERY_TIME { get; set; }
+        public string PRODUCT_REMARK { get; set; }
+        public string PRODUCT_FOR_GENDER { get; set; }
+        public string METAL_KARAT { get; set; }
+        public string METAL_COLOR { get; set; }
+        public string METAL_WEIGHT { get; set; }
+        public string KEYWORDS { get; set; }
+        public string CATEGORY_INFO { get; set; }
+        public string DESIGNER_COLLECTION { get; set; }
+        public string TOTAL_DIAMOND_CT_WT { get; set; }
+        //public decimal? TOTAL_DIAMOND_WT { get; set; }
+        public string STONE_TYPE { get; set; }
+        public string STONE_CARAT_WT { get; set; }
+        public string STONE_SHAPE { get; set; }
+        public string STONE_SIZE_RANGE { get; set; }
+        public string STONE_COLOR { get; set; }
+        public string STONE_CLARITY { get; set; }
+        public string STONE_QUALITY { get; set; }
+        public string NO_OF_SIDE_DIAMONDS { get; set; }
+        public string DIAMOND_CT_WT { get; set; }
+        public string DIAMOND_SHAPE { get; set; }
+        public string DIAMOND_COLOR { get; set; }
+        public string DIAMOND_CLARITY { get; set; }
+        public string COLOR_STONE_TYPE { get; set; }
+        public string COLOR_STONE_SHAPE { get; set; }
+        public string COLOR_STONE_SIZE_MM { get; set; }
+        public string COLOR_STONE_QUALITY { get; set; }
+        public string SETTING { get; set; }
+        public string FINISHING_TECHNIQUE { get; set; }
+        public string CHAIN_TYPE { get; set; }
+        public string CHAIN_LENGTH { get; set; }
+        //public string CHAINLENGTH { get; set; }
+        public string CLASP_TYPE { get; set; }
+        public string BACK_FINDING { get; set; }
+        public string SIZE_TYPE { get; set; }
+        public string AVAILABLE_SIZE_RANGE { get; set; }
+        public string STOCK_SIZES { get; set; }
+        public string RESIZING_CHARGES { get; set; }
+        public string META_TAG_TITLE { get; set; }
+        public string META_TAG_DESCRIPTION { get; set; }
+        public string META_TAG_KEYWORDS { get; set; }
+        public string RELATED_PRODUCTS { get; set; }//Related Product
+        public string SIMILAR_PRODUCTS { get; set; }//Related Product
+        public string Ring_Style { get; set; }
+        public float SELL_MARKUP { get; set; }
+
+
+        //******************Gift Specification****************************//
+        public string GIFT_TYPE { get; set; }
+        public string MATERIAL_1 { get; set; }
+        public string MATERIAL_2 { get; set; }
+        public string COLOR_1 { get; set; }
+        public string COLOR_2 { get; set; }
+        public string DIMENSION { get; set; }
+        public string WEIGHT { get; set; }
+        public string FINISH { get; set; }
+        public string MADE_IN { get; set; }
+        public string WARRANTY_TYPE { get; set; }
+        public string WARRANTY_DESCRIPTION { get; set; }
+
+
+        //******************Watch Specification****************************//
+        public string WATCH_TYPE { get; set; }
+        public string MODEL_NUMBER { get; set; }
+        public string DIAL_MATERIAL { get; set; }
+        public string DIAL_COLOR { get; set; }
+        public string SUBDIAL { get; set; }
+        public string CASE_MATERIAL { get; set; }
+        public string CASE_COLOR { get; set; }
+        public string CASE_DIAMETER { get; set; }
+        public string CASE_THICKNESS { get; set; }
+        public string BAND_MATERIAL { get; set; }
+        public string BAND_COLOR { get; set; }
+        public string BAND_LENGTH { get; set; }
+        public string BAND_WIDTH { get; set; }
+        public string BAND_TYPE { get; set; }
+        public string MOVEMENT { get; set; }
+        public string FUNCTION { get; set; }
+        public string WATER_RESISTANCE { get; set; }
+        public string CALENDAR { get; set; }
+        public string PRODUCT_WEIGHT { get; set; }
+        public string STATUS { get; set; }
+    }
+
+    }
